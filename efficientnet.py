057d00ee85c9716628e50daf6b629d7d01321107
1e100 <38598618+1e100@users.noreply.github.com>
2020-05-31 01:44:30 -0700

    Add Lite config

--- efficientnet.py
+++ efficientnet.py
@@ -17,14 +17,6 @@ _PYTORCH_VERSION = pkg_resources.parse_version(torch.__version__)
 _HAS_HARDSWISH = _PYTORCH_VERSION >= pkg_resources.parse_version("1.6.0")
 
 
-@dataclasses.dataclass
-class NetworkParams:
-    width_multiplier: float  # Channel multiplier.
-    depth_multiplier: float  # Number of layers multiplier.
-    resolution: int
-    dropout_fraction: float
-
-
 def sigmoid(x: torch.Tensor, inplace: bool = False) -> torch.Tensor:
     if inplace:
         return x.sigmoid_()
@@ -108,19 +100,37 @@ def _get_function(name: str, inplace: bool) -> nn.Module:
             return HardSigmoid(inplace=inplace)
 
 
+@dataclasses.dataclass
+class NetworkParams:
+    width_multiplier: float  # Channel multiplier.
+    depth_multiplier: float  # Number of layers multiplier.
+    resolution: int
+    dropout_fraction: float
+    activation_fn: str
+    se_gating_fn: str  # Only specified when use_se is true.
+    use_se: bool
+
+
 # "Official" values from here:
 # https://github.com/google/automl/blob/master/efficientdet/backbone/efficientnet_builder.py#L40
+# For Lite configs:
+# https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/lite/efficientnet_lite_builder.py#L35
 _NETWORK_PARAMS = {
-    "efficientnet-b0": NetworkParams(1.0, 1.0, 224, 0.2),
-    "efficientnet-b1": NetworkParams(1.0, 1.1, 240, 0.2),
-    "efficientnet-b2": NetworkParams(1.1, 1.2, 260, 0.3),
-    "efficientnet-b3": NetworkParams(1.2, 1.4, 300, 0.3),
-    "efficientnet-b4": NetworkParams(1.4, 1.8, 380, 0.4),
-    "efficientnet-b5": NetworkParams(1.6, 2.2, 456, 0.4),
-    "efficientnet-b6": NetworkParams(1.8, 2.6, 528, 0.5),
-    "efficientnet-b7": NetworkParams(2.0, 3.1, 600, 0.5),
-    "efficientnet-b8": NetworkParams(2.2, 3.6, 672, 0.5),
-    "efficientnet-l2": NetworkParams(4.3, 5.3, 800, 0.5),
+    "efficientnet-b0": NetworkParams(1.0, 1.0, 224, 0.2, "swish", "sigmoid", True),
+    "efficientnet-b1": NetworkParams(1.0, 1.1, 240, 0.2, "swish", "sigmoid", True),
+    "efficientnet-b2": NetworkParams(1.1, 1.2, 260, 0.3, "swish", "sigmoid", True),
+    "efficientnet-b3": NetworkParams(1.2, 1.4, 300, 0.3, "swish", "sigmoid", True),
+    "efficientnet-b4": NetworkParams(1.4, 1.8, 380, 0.4, "swish", "sigmoid", True),
+    "efficientnet-b5": NetworkParams(1.6, 2.2, 456, 0.4, "swish", "sigmoid", True),
+    "efficientnet-b6": NetworkParams(1.8, 2.6, 528, 0.5, "swish", "sigmoid", True),
+    "efficientnet-b7": NetworkParams(2.0, 3.1, 600, 0.5, "swish", "sigmoid", True),
+    "efficientnet-b8": NetworkParams(2.2, 3.6, 672, 0.5, "swish", "sigmoid", True),
+    "efficientnet-l2": NetworkParams(4.3, 5.3, 800, 0.5, "swish", "sigmoid", True),
+    "efficientnet-lite0": NetworkParams(1.0, 1.0, 224, 0.2, "relu6", "", False),
+    "efficientnet-lite1": NetworkParams(1.0, 1.1, 240, 0.2, "relu6", "", False),
+    "efficientnet-lite2": NetworkParams(1.1, 1.2, 260, 0.3, "relu6", "", False),
+    "efficientnet-lite3": NetworkParams(1.2, 1.4, 280, 0.3, "relu6", "", False),
+    "efficientnet-lite4": NetworkParams(1.4, 1.8, 300, 0.3, "relu6", "", False),
 }
 
 
@@ -194,7 +204,7 @@ class _InvertedResidual(nn.Module):
         super().__init__()
         assert stride in [1, 2]
         assert kernel_size in [3, 5]
-        mid_ch = in_ch * expand_ratio
+        mid_ch = _round_to_8(in_ch * expand_ratio)
         self.apply_residual = allow_residual and (in_ch == out_ch and stride == 1)
 
         layers = []
@@ -246,15 +256,29 @@ class _InvertedResidual(nn.Module):
             return self.layers(input)
 
 
-class EfficientNet(nn.Module):
+class Classifier(nn.Sequential):
     def __init__(
         self,
-        in_ch: int = 3,
-        num_classes: int = 1000,
-        variant: str = "efficientnet-b0",
-        activation_fn: str = "swish",
-        se_gating_fn: str = "sigmoid",
-        allow_se: bool = True,  # Currently unused
+        in_ch: int,
+        mid_ch: int,
+        num_classes: int,
+        activation_fn: str,
+        dropout_rate: float,
+    ) -> None:
+        super().__init__(
+            nn.Conv2d(in_ch, mid_ch, kernel_size=1, stride=1, bias=False,),
+            nn.BatchNorm2d(mid_ch, momentum=_BN_MOMENTUM, eps=_BN_EPSILON),
+            _get_function(activation_fn, inplace=True),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Dropout(dropout_rate, inplace=True),
+            nn.Linear(mid_ch, num_classes, bias=True),
+        )
+
+
+class EfficientNet(nn.Module):
+    def __init__(
+        self, in_ch: int = 3, num_classes: int = 1000, variant: str = "efficientnet-b0",
     ) -> None:
         """ Usage:
         >>> model = EfficientNet(1, num_classes=1000, variant="efficientnet-b3")
@@ -272,72 +296,52 @@ class EfficientNet(nn.Module):
         width_multiplier = network_params.width_multiplier
         self.optimal_resolution = network_params.resolution
 
+        # For "lite" variants, stem width is not scaled with the rest of the net.
+        stem_width_multiplier = width_multiplier if "lite" not in variant else 1.0
+        stem_out_ch = _round_to_8(_BLOCK_PARAMS[0].in_ch * stem_width_multiplier)
         self.stem = nn.Sequential(
             nn.Conv2d(
-                in_ch,
-                _round_to_8(_BLOCK_PARAMS[0].in_ch * width_multiplier),
-                kernel_size=3,
-                stride=2,
-                padding=1,
-                bias=False,
+                in_ch, stem_out_ch, kernel_size=3, stride=2, padding=1, bias=False,
             ),
-            nn.BatchNorm2d(
-                _round_to_8(_BLOCK_PARAMS[0].in_ch * width_multiplier),
-                momentum=_BN_MOMENTUM,
-                eps=_BN_EPSILON,
-            ),
-            _get_function(activation_fn, inplace=True),
+            nn.BatchNorm2d(stem_out_ch, momentum=_BN_MOMENTUM, eps=_BN_EPSILON,),
+            _get_function(network_params.activation_fn, inplace=True),
         )
 
         depth_multiplier = network_params.depth_multiplier
         blocks = collections.OrderedDict()
         for block_idx, block_params in enumerate(_BLOCK_PARAMS):
             repeats = math.ceil(block_params.repeats * depth_multiplier)
-            in_ch = _round_to_8(block_params.in_ch * width_multiplier)
+            in_ch = (
+                stem_out_ch
+                if block_idx == 0
+                else _round_to_8(block_params.in_ch * width_multiplier)
+            )
             out_ch = _round_to_8(block_params.out_ch * width_multiplier)
             block_layers = collections.OrderedDict()
-            block_layers["ir0"] = _InvertedResidual(
-                in_ch,
-                out_ch,
-                kernel_size=block_params.kernel_size,
-                stride=block_params.stride,
-                expand_ratio=block_params.expand_ratio,
-                se_ratio=block_params.se_ratio,
-                activation_fn=activation_fn,
-                se_gating_fn=se_gating_fn,
-            )
-            for layer_idx in range(1, repeats):
+            for layer_idx in range(repeats):
                 block_layers[f"ir{layer_idx}"] = _InvertedResidual(
-                    out_ch,
+                    in_ch if layer_idx == 0 else out_ch,
                     out_ch,
                     kernel_size=block_params.kernel_size,
-                    stride=1,
+                    stride=block_params.stride if layer_idx == 0 else 1,
                     expand_ratio=block_params.expand_ratio,
-                    se_ratio=block_params.se_ratio,
-                    activation_fn=activation_fn,
-                    se_gating_fn=se_gating_fn,
+                    se_ratio=block_params.se_ratio if network_params.use_se else None,
+                    activation_fn=network_params.activation_fn,
+                    se_gating_fn=network_params.se_gating_fn,
                 )
             blocks[f"block{block_idx}"] = nn.Sequential(block_layers)
 
         self.body = nn.Sequential(blocks)
 
-        self.classifier = nn.Sequential(
-            nn.Conv2d(
-                _round_to_8(_BLOCK_PARAMS[-1].out_ch * width_multiplier),
-                1280,
-                kernel_size=1,
-                stride=1,
-                bias=False,
-            ),
-            nn.BatchNorm2d(1280, momentum=_BN_MOMENTUM, eps=_BN_EPSILON),
-            _get_function(activation_fn, inplace=True),
-            nn.AdaptiveAvgPool2d(1),
-            nn.Dropout(network_params.dropout_fraction, inplace=True),
-            nn.Flatten(),
-            nn.Linear(1280, num_classes, bias=True),
+        self.classifier = Classifier(
+            _round_to_8(_BLOCK_PARAMS[-1].out_ch * width_multiplier),
+            1280,
+            num_classes,
+            network_params.activation_fn,
+            network_params.dropout_fraction,
         )
 
-        self.init_weights()
+        self.init_weights_google()
 
     def init_weights(self):
         for name, module in self.named_modules():
@@ -346,14 +350,45 @@ class EfficientNet(nn.Module):
                 nn.init.kaiming_normal_(
                     module.weight, mode="fan_out", nonlinearity="relu"
                 )
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
             elif isinstance(module, nn.BatchNorm2d):
-                module.weight.data.fill_(1.0)
-                module.bias.data.zero_()
+                nn.init.ones_(module.weight)
+                nn.init.zeros_(module.bias)
             elif isinstance(module, nn.Linear):
                 # Normally this is uniform_ bounded to a computed value.
                 nn.init.kaiming_uniform_(
                     module.weight, mode="fan_in", nonlinearity="linear"
                 )
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
+
+    def init_weights_google(self):
+        """ NOTE: ported from, with corrections
+        https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/efficientnet_model.py#L61
+        """
+        for name, module in self.named_modules():
+            if isinstance(module, nn.Conv2d):
+                fan_out = (
+                    int(
+                        module.kernel_size[0]
+                        * module.kernel_size[1]
+                        * module.out_channels
+                    )
+                    // module.groups
+                )
+                nn.init.normal_(module.weight, mean=0.0, std=math.sqrt(2.0 / fan_out))
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.BatchNorm2d):
+                nn.init.ones_(module.weight)
+                nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.Linear):
+                fan_out = module.weight.size(0)
+                range = 1.0 / math.sqrt(fan_out)
+                nn.init.uniform_(module.weight, -range, range)
+                if module.bias is not None:
+                    nn.init.zeros_(module.bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.stem(x)
